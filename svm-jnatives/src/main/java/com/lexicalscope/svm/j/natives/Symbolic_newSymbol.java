855bbdbc4fb018f97e2eea94c449aaa866b5932d
Artur Spychaj <drognanar@gmail.com>
2015-04-23 11:12:20 +0200

    newSymbol method now creates unique symbols

--- svm-jnatives/src/main/java/com/lexicalscope/svm/j/natives/Symbolic_newSymbol.java
+++ svm-jnatives/src/main/java/com/lexicalscope/svm/j/natives/Symbolic_newSymbol.java
@@ -11,17 +11,37 @@ import static com.lexicalscope.svm.j.statementBuilder.StatementBuilder.statement
  * Class that allows dynamic creation of int symbols in execution.
  */
 public class Symbolic_newSymbol extends AbstractNativeMethodDef {
+    private static int counter = 0;
+
     public Symbolic_newSymbol(String methodName, String signature) {
         super("com/lexicalscope/svm/j/instruction/symbolic/symbols/SymbolFactory", methodName, signature);
     }
 
     @Override
     public MethodBody instructions(InstructionSource instructions) {
-        ITerminalSymbol symbol = new ITerminalSymbol("s1"); // needs to be a proper symbol.
+        ITerminalSymbol symbol = getUniqueSymbol();
         return statements(instructions)
                 .maxStack(1)
                 .maxLocals(1)
                 .linearOp(new LoadConstantArg(symbol))
                 .return1(name()).build();
+    }
+
+    /**
+     * Returns a symbol with a unique name.
+     * @return A fresh symbol with a unique name.
+     */
+    public ITerminalSymbol getUniqueSymbol() {
+        return getUniqueSymbol("symbol");
+    }
+
+    /**
+     * Returns a symbol with a unique name.
+     * @return A fresh symbol with a unique name.
+     */
+    public ITerminalSymbol getUniqueSymbol(String prefix) {
+        ITerminalSymbol symbol = new ITerminalSymbol(String.format("%s%d", prefix, counter));
+        counter++;
+        return symbol;
     }
 }
