43c5629a3ed833350a9034c9053071bd17a1bc1c
Artur Spychaj <drognanar@gmail.com>
2015-04-23 11:12:42 +0200

    Implements the selectState method

--- svm-jnatives/src/main/java/com/lexicalscope/svm/j/natives/Symbolic_selectState.java
+++ svm-jnatives/src/main/java/com/lexicalscope/svm/j/natives/Symbolic_selectState.java
@@ -1,12 +1,16 @@
 package com.lexicalscope.svm.j.natives;
 
-import com.lexicalscope.svm.j.instruction.concrete.LoadConstantArg;
 import com.lexicalscope.svm.j.instruction.factory.InstructionSource;
-import com.lexicalscope.svm.j.instruction.symbolic.symbols.ITerminalSymbol;
+import com.lexicalscope.svm.vm.j.InstructionQuery;
+import com.lexicalscope.svm.vm.j.JState;
 import com.lexicalscope.svm.vm.j.MethodBody;
+import com.lexicalscope.svm.vm.j.Vop;
 
 import static com.lexicalscope.svm.j.statementBuilder.StatementBuilder.statements;
 
+/**
+ * Method which chooses a number randomly between 0 and (count - 1) creating n choices.
+ */
 public class Symbolic_selectState extends AbstractNativeMethodDef {
     public Symbolic_selectState() {
         super("com/lexicalscope/svm/j/instruction/symbolic/symbols/SymbolFactory", "selectState", "(I)I");
@@ -14,11 +18,28 @@ public class Symbolic_selectState extends AbstractNativeMethodDef {
 
     @Override
     public MethodBody instructions(InstructionSource instructions) {
-        ITerminalSymbol symbol = new ITerminalSymbol("s1"); // needs to be a proper symbol.
         return statements(instructions)
                 .maxStack(1)
                 .maxLocals(1)
-                .linearOp(new LoadConstantArg(symbol))
+                .linearOp(new SelectStateOp())
                 .return1(name()).build();
+    }
+
+    private class SelectStateOp implements Vop {
+        @Override
+        public void eval(JState ctx) {
+            int count = (int) ctx.pop();
+            JState[] forks = new JState[count];
+            for (int i = 0; i < count; i++) {
+                forks[i] = ctx.snapshot();
+                forks[i].push(i);
+            }
+            ctx.fork(forks);
+        }
+
+        @Override
+        public <T> T query(InstructionQuery<T> instructionQuery) {
+            return instructionQuery.nativ3();
+        }
     }
 }
