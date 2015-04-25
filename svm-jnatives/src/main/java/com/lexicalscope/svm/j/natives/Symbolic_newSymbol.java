5f440cff1a93cb4c438de1a4262b02926f4da10a
Artur Spychaj <drognanar@gmail.com>
2015-04-25 20:49:17 +0200

    Adds a counter meta state for symbol reference counting
    Using a static field makes all symbols unique whereas we only want symbols in one execution to be unique.

--- svm-jnatives/src/main/java/com/lexicalscope/svm/j/natives/Symbolic_newSymbol.java
+++ svm-jnatives/src/main/java/com/lexicalscope/svm/j/natives/Symbolic_newSymbol.java
@@ -1,47 +1,44 @@
 package com.lexicalscope.svm.j.natives;
 
-import com.lexicalscope.svm.j.instruction.concrete.LoadConstantArg;
 import com.lexicalscope.svm.j.instruction.factory.InstructionSource;
 import com.lexicalscope.svm.j.instruction.symbolic.symbols.ITerminalSymbol;
+import com.lexicalscope.svm.vm.j.InstructionQuery;
+import com.lexicalscope.svm.vm.j.JState;
 import com.lexicalscope.svm.vm.j.MethodBody;
+import com.lexicalscope.svm.vm.j.Vop;
 
 import static com.lexicalscope.svm.j.statementBuilder.StatementBuilder.statements;
+import static com.lexicalscope.svm.j.instruction.concrete.object.SymbolCounterMetaKey.SC;
 
 /**
  * Class that allows dynamic creation of int symbols in execution.
  */
 public class Symbolic_newSymbol extends AbstractNativeMethodDef {
-    private static int counter = 0;
-
     public Symbolic_newSymbol(String methodName, String signature) {
         super("com/lexicalscope/svm/j/instruction/symbolic/symbols/SymbolFactory", methodName, signature);
     }
 
     @Override
     public MethodBody instructions(InstructionSource instructions) {
-        ITerminalSymbol symbol = getUniqueSymbol();
         return statements(instructions)
                 .maxStack(1)
                 .maxLocals(1)
-                .linearOp(new LoadConstantArg(symbol))
+                .linearOp(new GetSymbolOp())
                 .return1(name()).build();
     }
 
-    /**
-     * Returns a symbol with a unique name.
-     * @return A fresh symbol with a unique name.
-     */
-    public ITerminalSymbol getUniqueSymbol() {
-        return getUniqueSymbol("symbol");
-    }
+    private class GetSymbolOp implements Vop {
+        @Override
+        public void eval(JState ctx) {
+            int counter = ctx.getMeta(SC);
+            ITerminalSymbol symbol = new ITerminalSymbol(String.format("%s%d", "symbol", counter));
+            ctx.setMeta(SC, counter + 1);
+            ctx.push(symbol);
+        }
 
-    /**
-     * Returns a symbol with a unique name.
-     * @return A fresh symbol with a unique name.
-     */
-    public ITerminalSymbol getUniqueSymbol(String prefix) {
-        ITerminalSymbol symbol = new ITerminalSymbol(String.format("%s%d", prefix, counter));
-        counter++;
-        return symbol;
+        @Override
+        public <T> T query(InstructionQuery<T> instructionQuery) {
+            return instructionQuery.nativ3();
+        }
     }
 }
