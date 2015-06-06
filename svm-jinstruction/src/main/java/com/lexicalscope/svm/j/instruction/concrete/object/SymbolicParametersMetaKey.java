54c0c09486e68bc58954a3bdcd35e7ced326e599
Artur Spychaj <drognanar@gmail.com>
2015-06-06 19:31:57 +0200

    Makes the symbolic parameters map immutable
    Because it is uncertain whether the S_PARAMETERS map is shared the
    entire data structure has to be copied every single time.

--- svm-jinstruction/src/main/java/com/lexicalscope/svm/j/instruction/concrete/object/SymbolicParametersMetaKey.java
+++ svm-jinstruction/src/main/java/com/lexicalscope/svm/j/instruction/concrete/object/SymbolicParametersMetaKey.java
@@ -14,19 +14,28 @@ public class SymbolicParametersMetaKey implements MetaKey<Map> {
     public static final SymbolicParametersMetaKey S_PARAMETERS = new SymbolicParametersMetaKey();
 
     public static Map addMapping(Map passedParameters, Object receiver, Object passedClass, Object passedObject) {
-        HashMap originalMap = (HashMap) passedParameters;
-        Map resultMap = (Map) originalMap.clone();
-        if (!resultMap.containsKey(receiver)) {
-            resultMap.put(receiver, new HashMap());
-        }
-        Map passedObjectParameters = (Map) resultMap.get(receiver);
-        if (!passedObjectParameters.containsKey(passedClass)) {
-            passedObjectParameters.put(passedClass, new ArrayList());
-        }
+        HashMap classToObjectMapping = copyOrCreateMap(passedParameters);
+        HashMap objectToInstancesMapping = copyOrCreateMap(passedParameters.get(receiver));
+        ArrayList instancesMapping = copyOrCreateList(objectToInstancesMapping.get(passedClass));
 
-        ArrayList possibleValues = (ArrayList) passedObjectParameters.get(passedClass);
-        possibleValues.add(passedObject);
-        return resultMap;
+        instancesMapping.add(passedObject);
+        objectToInstancesMapping.put(passedClass, instancesMapping);
+        classToObjectMapping.put(receiver, objectToInstancesMapping);
+        return classToObjectMapping;
+    }
+
+    private static HashMap copyOrCreateMap(Object o) {
+        Map map = (Map) o;
+        return map == null
+                ? new HashMap()
+                : new HashMap(map);
+    }
+
+    private static ArrayList copyOrCreateList(Object o) {
+        ArrayList list = (ArrayList) o;
+        return list == null
+                ? new ArrayList()
+                : new ArrayList(list);
     }
 
     public static int countObjects(Map passedParameters, Object receiver, Object passedClass) {
